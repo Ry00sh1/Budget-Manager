075100ba021f1f7f1c7b37b4e48082da7d2012c4
Pedro Sena <phasena@gmail.com>
2025-01-07 14:06:23 +0000

    feat: some fields of Company model

--- budget_manager/companies/models.py
+++ budget_manager/companies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-class Employer(models.Model):
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=14)
+    email = models.EmailField()
+    cep = models.CharField(max_length=8)
+    street = models.JSONField()
+    district = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
